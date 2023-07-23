d63394c81392e1d76f18db995a2f50fa72ad6001
xNewz <63030068@kmitl.ac.th>
2023-07-23 21:00:00 +0700

    change: graphql server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,8 +23,8 @@ const main = async () => {
         schema,
         graphiql: true
     }));
-    app.listen(3000, () => {
-        console.log("Server started on http://localhost:3000");
+    app.listen(3001, () => {
+        console.log("Server started on http://localhost:3001");
     });
 };
 
